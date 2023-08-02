b2f984f898cf438d90ae57129bea1cd1c456dc1e
jisooround <9utesleben@gmail.com>
2023-08-02 20:07:09 +0900

    design: button 컴포넌트 스타일

---
+++ src/components/Button.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+type Props = {
+  content: string;
+};
+
+const Button = ({ content }: Props) => {
+  return <ButtonStyle>{content}</ButtonStyle>;
+};
+
+const ButtonStyle = styled.button`
+  border-radius: 5px;
+  padding: 8px 24px;
+  line-height: 1.8;
+  font-size: 14px;
+  background-color: var(--color-primary);
+  color: var(--color-white);
+  &:hover {
+    background-color: var(--color-primary-dark);
+  }
+`;
+
+export default Button;
--- src/global/globalStyles.ts
+++ src/global/globalStyles.ts
@@ -3,6 +3,33 @@ import reset from "styled-reset";
 
 const GlobalStyles = createGlobalStyle`
 ${reset}
+
+body{
+  button{
+    border: none;
+    background-color: transparent;
+  }
+}
+
+:root {
+  //color
+  --color-white: #FFFFFF;
+  --color-black : #333333;
+  --color-primary: #024EEE;
+  --color-primary-dark: #003AD6;
+  --color-primary-light: #DBE1FD;
+  --color-gray-border: #DEDEDE;
+  --color-gray-001: #F6F7F8;
+  --color-gray-002: #F0F2F3;
+  --color-gray-003: #E0E4E8;
+  --color-gray-004: #E0E4E8;
+  --color-gray-005: #BEBEBE;
+  --color-gray-006: #9BA1A7;
+  --color-gray-007: #9C9C9C;
+  --color-gray-008: #74797D;
+  --color-gray-009: #565656;
+  --color-pink: #FF00FF;
+}
 `;
 
 export default GlobalStyles;

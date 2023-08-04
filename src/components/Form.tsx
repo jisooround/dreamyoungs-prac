8fd5bbfd606bb04de19d686d6b1b18c75f1f6931
jisooround <9utesleben@gmail.com>
2023-08-05 01:43:00 +0900

    feat: 라디오, 체크박스 컴포넌트 추가

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,47 +2,45 @@ import styled from "styled-components";
 import FormFixedType from "./FormFixedType";
 import FormTextType from "./FormTextType";
 import FormDateType from "./FormDateType";
+import FormRadioType from "./FormRadioType";
+import Button from "../components/Button";
+import FormCheckType from "./FormCheckType";
 
-const Form = () => (
-  <FormStyle>
-    <FormFixedType label={"정보 1"} value={"정보 내용"} />
-    <FormTextType label={"정보 2"} value={"정보 내용"} />
-    <FormFixedType label={"정보 3"} value={"정보 내용"} />
-    <FormTextType label={"정보 4"} value={"정보 내용"} />
-    <FormDateType label={"날짜"} value={"2012-03-13"} />
-    <div>
-      <label>정보5</label>
-      <RadioBoxStyle>
-        <input type="radio" id="선택1" value="선택1" name="info5" />
-        <label className="group_input" htmlFor="선택1">
-          선택1
-        </label>
-        <input type="radio" id="선택2" value="선택2" name="info5" />
-        <label className="group_input" htmlFor="선택2">
-          선택2
-        </label>
-        <input type="radio" id="선택3" value="선택3" name="info5" />
-        <label className="group_input" htmlFor="선택3">
-          선택3
-        </label>
-      </RadioBoxStyle>
-    </div>
-  </FormStyle>
-);
+const Form = () => {
+  const submitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    console.log(formData.get("정보 5"));
+  };
+
+  const radioOptionList = ["선택1", "선택2", "선택3"];
+
+  return (
+    <FormStyle onSubmit={submitHandler}>
+      <FormFixedType label={"정보 1"} value={"정보 내용"} />
+      <FormTextType label={"정보 2"} value={"정보 내용"} />
+      <FormFixedType label={"정보 3"} value={"정보 내용"} />
+      <FormTextType label={"정보 4"} value={"정보 내용"} />
+      <FormDateType label={"날짜"} value={"2012-03-13"} />
+      <FormRadioType
+        options={radioOptionList}
+        label={"정보 5"}
+        value={"선택1"}
+      />
+      <FormCheckType
+        options={radioOptionList}
+        label={"정보 6"}
+        value={"선택1"}
+      />
+      <Button content={"저장"} />
+    </FormStyle>
+  );
+};
 
 const FormStyle = styled.form`
   margin: 0 2.5rem;
   border-top: 1px solid var(--color-gray-003);
-`;
-
-const RadioBoxStyle = styled.div`
-  input {
-    padding-right: 20px;
-  }
-  label {
-    width: 20px;
-    padding-right: 20px;
-  }
 `;
 
 export default Form;

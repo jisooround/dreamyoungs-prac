4fdf33c51d0ca84f50967d46b3573509761da49f
jisooround <9utesleben@gmail.com>
2023-08-08 17:15:10 +0900

    feat: api 컨트롤

--- src/api/apiControl.ts
+++ src/api/apiControl.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
+import { IFormData } from "../types/data";
 
 const BASE_URL = "https://api-jobtest.json2bot.chat/";
 const HEADERS = {
   "Content-Type": "application/json",
 };
 
-class getApi {
+class apiControl {
   axiosClient;
   constructor() {
     this.axiosClient = axios.create({
@@ -23,6 +24,18 @@ class getApi {
       throw error;
     }
   }
+
+  async postData(data: IFormData) {
+    try {
+      console.log("data", data);
+      const response = await this.axiosClient.post(`/test`, data);
+      console.log(response.data);
+      return response;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
 }
 
-export const api = new getApi();
+export const api = new apiControl();
